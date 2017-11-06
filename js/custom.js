67449945d64ce32560d179a9ebf08bf7a50d2443
Oyamasiphula <oyamasiphula@gmail.com>
2017-11-05 22:08:50 -0800

    added more functionality for my nav and for the projects section

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,40 @@
+// nav bar 
+var personalStatement = document.getElementById("personalStatement");
+var projectsContent = document.getElementById("projectsSection");
+var selector = ".topnav li"
+
+$(selector).on('click', function(){
+    $(selector).removeClass('active');
+    $(this).addClass('active');
+}); 
+
+home.addEventListener("click", function(){
+	personalStatement.classList.remove("hide");
+	projectsContent.classList.add("hide");
+
+});
+
+projects.addEventListener("click", function(){
+	personalStatement.classList.add("hide");
+	projectsContent.classList.remove("hide");
+});
+
+contact.addEventListener("click", function(){
+
+});
+
+about.addEventListener("click", function(){
+
+});
+// nav bar 
+
+
+
+// <tab function>
+ $( function() {
+    $( "#tabs" ).tabs();
+  } );
+// <tab function>
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -6,7 +43,6 @@ function myFunction() {
         x.className = "topnav";
     }
 }
-
 $(function() {
 	var message = {
 
