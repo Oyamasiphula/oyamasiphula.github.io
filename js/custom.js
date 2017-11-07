bdd25f25e624e70760c46c0141194e81c7310afd
Oyamasiphula <oyamasiphula@gmail.com>
2017-11-07 03:07:38 -0800

    added a modularised code

--- js/custom.js
+++ js/custom.js
@@ -1,40 +1,9 @@
-// nav bar 
-var personalStatement = document.getElementById("personalStatement");
-var projectsContent = document.getElementById("projectsSection");
-var selector = ".topnav li"
-
-$(selector).on('click', function(){
-    $(selector).removeClass('active');
-    $(this).addClass('active');
-}); 
-
-home.addEventListener("click", function(){
-	personalStatement.classList.remove("hide");
-	projectsContent.classList.add("hide");
-
-});
-
-projects.addEventListener("click", function(){
-	personalStatement.classList.add("hide");
-	projectsContent.classList.remove("hide");
-});
-
-contact.addEventListener("click", function(){
-
-});
-
-about.addEventListener("click", function(){
-
-});
-// nav bar 
-
-
-
 // <tab function>
  $( function() {
     $( "#tabs" ).tabs();
   } );
 // <tab function>
+
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
