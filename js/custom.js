0ebe8f1cc9b8d38be157071fc0d721a42d69b5f9
oyamasiphula <oyamasiphula@gmail.com>
2017-11-10 13:48:53 +0200

    added aa beautified code

--- js/custom.js
+++ js/custom.js
@@ -1,60 +1,61 @@
 // <tab function>
- $( function() {
-    $( "#tabs" ).tabs();
-  } );
+$(function() {
+  $("#tabs").tabs();
+});
 // <tab function>
 
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
 $(function() {
-	var message = {
+  var message = {
 
-		message: [
-			'web development;', 
-			'eye candy design;', 
-			'good movie and popcorn;', 
-      		'javascript, html, css, nodejs...;'
-		],
-		counterS: 0,
-		counterL: 0,
-		deleteS: false,
+    message: [
+      'web development;',
+      'eye candy design;',
+      'good movie and popcorn;',
+      'javascript, html, css, nodejs...;'
+    ],
+    counterS: 0,
+    counterL: 0,
+    deleteS: false,
 
-		init: function() {
-			this.cacheElem();
-			this.type();
-		},
+    init: function() {
+      this.cacheElem();
+      this.type();
+    },
 
-		cacheElem: function() {
-			this.$text = $('.text');
-		},
+    cacheElem: function() {
+      this.$text = $('.text');
+    },
 
-		type: function() {
-			var message = this.message[this.counterS],
-				  that = this,
-				  speed = 0;
+    type: function() {
+      var message = this.message[this.counterS],
+        that = this,
+        speed = 0;
 
-			message = !this.deleteS ? message.slice(0, ++this.counterL) : message.slice(0, --this.counterL);
-			if(this.message[this.counterS] != message && !this.deleteS) {
-				this.$text.text(message);
-				speed = 90;
-			}
-			else {
-				this.deleteS = true;
-				speed = this.message[this.counterS] == message ? 1000 : 40;
-				this.$text.text(message);
-				if (message == '') {
-					this.deleteS = false;
-					this.counterS = this.counterS < this.message.length - 1 ? this.counterS + 1 : 0;
-				}
-			}
-			setTimeout(function(){that.type()}, speed);
-		}
-	};
-	message.init();
+      message = !this.deleteS ? message.slice(0, ++this.counterL) : message.slice(0, --this.counterL);
+      if (this.message[this.counterS] != message && !this.deleteS) {
+        this.$text.text(message);
+        speed = 90;
+      } else {
+        this.deleteS = true;
+        speed = this.message[this.counterS] == message ? 1000 : 40;
+        this.$text.text(message);
+        if (message == '') {
+          this.deleteS = false;
+          this.counterS = this.counterS < this.message.length - 1 ? this.counterS + 1 : 0;
+        }
+      }
+      setTimeout(function() {
+        that.type()
+      }, speed);
+    }
+  };
+  message.init();
 });
