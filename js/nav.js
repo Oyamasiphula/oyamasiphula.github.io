a4f2e35272a8e661ef6dd52c3dcd0c0435f27730
oyamasiphula <oyamasiphula@gmail.com>
2017-11-10 12:33:29 +0200

    added functionality that fits with the navigation bar div section appearance

--- js/nav.js
+++ js/nav.js
@@ -1,30 +1,34 @@
-// nav bar 
+// nav bar
 var personalStatement = document.getElementById("personalStatement");
 var projectsDivContent = document.getElementById("projectsSection");
+var contactDivCont = document.getElementById("contactSection");
 
 var selector = ".topnav li"
 
-$(selector).on('click', function(){
-    $(selector).removeClass('active');
-    $(this).addClass('active');
-}); 
-
-home.addEventListener("click", function(){
-	personalStatement.classList.remove("hide");
-	projectsDivContent.classList.add("hide");
-
+$(selector).on('click', function() {
+  $(selector).removeClass('active');
+  $(this).addClass('active');
 });
 
-projects.addEventListener("click", function(){
-	personalStatement.classList.add("hide");
-	projectsDivContent.classList.remove("hide");
+home.addEventListener("click", function() {
+  projectsDivContent.classList.add("hide");
+  contactDivCont.classList.add("hide");
+  personalStatement.classList.remove("hide");
 });
 
-contact.addEventListener("click", function(){
-
+projects.addEventListener("click", function() {
+  personalStatement.classList.add("hide");
+  contactDivCont.classList.add("hide");
+  projectsDivContent.classList.remove("hide");
 });
 
-about.addEventListener("click", function(){
+contact.addEventListener("click", function() {
+  personalStatement.classList.add("hide");
+  projectsDivContent.classList.add("hide");
+  contactDivCont.classList.remove("hide");
+});
+
+about.addEventListener("click", function() {
 
 });
-// nav bar 
+// nav bar
