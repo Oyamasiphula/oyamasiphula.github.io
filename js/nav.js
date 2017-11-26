782818c379c2421b02696fcd32af998668470a7b
Oyamasiphula <oyamasiphula@gmail.com>
2017-11-25 20:50:32 -0800

    added the about content flow of appearance

--- js/nav.js
+++ js/nav.js
@@ -2,6 +2,7 @@
 var personalStatement = document.getElementById("personalStatement");
 var projectsDivContent = document.getElementById("projectsSection");
 var contactDivCont = document.getElementById("contactSection");
+var aboutMeDivCont = document.getElementById("aboutMeSection");
 
 var selector = ".topnav li"
 
@@ -13,22 +14,28 @@ $(selector).on('click', function() {
 home.addEventListener("click", function() {
   projectsDivContent.classList.add("hide");
   contactDivCont.classList.add("hide");
+  aboutMeSection.classList.add("hide");
   personalStatement.classList.remove("hide");
 });
 
 projects.addEventListener("click", function() {
   personalStatement.classList.add("hide");
   contactDivCont.classList.add("hide");
+  aboutMeSection.classList.add("hide");
   projectsDivContent.classList.remove("hide");
 });
 
 contact.addEventListener("click", function() {
   personalStatement.classList.add("hide");
   projectsDivContent.classList.add("hide");
+  aboutMeSection.classList.add("hide");
   contactDivCont.classList.remove("hide");
 });
 
 about.addEventListener("click", function() {
-
+  personalStatement.classList.add("hide");
+  projectsDivContent.classList.add("hide");
+  contactDivCont.classList.add("hide");
+  aboutMeSection.classList.remove("hide");
 });
 // nav bar
