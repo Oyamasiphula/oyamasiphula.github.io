5b6bdce014a70a35b523e75d8bd71f603dc76209
Oyamasiphula <oyamasiphula@gmail.com>
2017-11-27 17:53:37 -0800

    added my skill's progress bar functionality

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,10 @@
-// <tab function>
+// [<tab function>
 $(function() {
   $("#tabs").tabs();
 });
-// <tab function>
+// <tab function>]
 
+// [<top nav bar>
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -12,6 +13,9 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+// <top nav bar>]
+
+// [<typing animation>
 $(function() {
   var message = {
 
@@ -59,3 +63,53 @@ $(function() {
   };
   message.init();
 });
+// <typing animation>]
+
+// [<about skills>
+;( function( $ ) {
+  "use strict";
+  
+  var $bars = $( ".bar" ),
+    methods = {
+      init: function() {
+        
+        // Bind events
+        methods.bindEvents();
+        
+      },
+      bindEvents: function() {
+
+        // Loop through each of the bars...
+        $bars.each( function() {
+
+          var $bar = $( this ),
+            $pct = $bar.find( ".pct" ),
+            data = $bar.data( "bar" );
+
+          setTimeout( function() {
+
+            $bar
+              .css( "background-color", data.color )
+              .animate({
+                "width": $pct.html()
+              }, data.speed || 3000, function() {
+
+                $pct.css({
+                  "color": data.color,
+                  "opacity": 1
+                });
+
+              });
+
+          }, data.delay || 0 );     
+
+        });
+
+      }
+    };
+  
+  // Initialize on page load
+  methods.init(); 
+    
+})( jQuery );
+// <about skills>]
