4895c15a385a86c22f30336cb8474afb03bab2de
Oyamasiphula <oyamasiphula@gmail.com>
2017-11-27 17:53:38 -0800

    added the about page button navigation eventHandlers

--- js/nav.js
+++ js/nav.js
@@ -2,9 +2,18 @@
 var personalStatement = document.getElementById("personalStatement");
 var projectsDivContent = document.getElementById("projectsSection");
 var contactDivCont = document.getElementById("contactSection");
-var aboutMeDivCont = document.getElementById("aboutMeSection");
+var aboutMeDivCont = document.querySelectorAll("#aboutMeSection");
+var softSKillDivCont = document.getElementById("container");
+var infoTap = document.getElementById("info-tap");
+var softTap = document.getElementById("soft-tap");
 
 var selector = ".topnav li"
+
+function hideAboutCont(){
+  aboutMeSection[0].classList.add("hide");
+  aboutMeSection[1].classList.add("hide");
+};
+
 
 $(selector).on('click', function() {
   $(selector).removeClass('active');
@@ -12,30 +21,40 @@ $(selector).on('click', function() {
 });
 
 home.addEventListener("click", function() {
+  hideAboutCont();
   projectsDivContent.classList.add("hide");
   contactDivCont.classList.add("hide");
-  aboutMeSection.classList.add("hide");
   personalStatement.classList.remove("hide");
 });
 
 projects.addEventListener("click", function() {
+  hideAboutCont();
   personalStatement.classList.add("hide");
   contactDivCont.classList.add("hide");
-  aboutMeSection.classList.add("hide");
   projectsDivContent.classList.remove("hide");
 });
 
 contact.addEventListener("click", function() {
+  hideAboutCont();
   personalStatement.classList.add("hide");
   projectsDivContent.classList.add("hide");
-  aboutMeSection.classList.add("hide");
   contactDivCont.classList.remove("hide");
 });
 
 about.addEventListener("click", function() {
   personalStatement.classList.add("hide");
-  projectsDivContent.classList.add("hide");
   contactDivCont.classList.add("hide");
-  aboutMeSection.classList.remove("hide");
+  projectsDivContent.classList.add("hide");
+  aboutMeSection[0].classList.remove("hide");
+  aboutMeSection[1].classList.remove("hide");
 });
 // nav bar
+
+infoTap.addEventListener("click", function() {
+  softSKillDivCont.classList.add("hide");
+});
+softTap.addEventListener("click", function() {
+  personalStatement.classList.add("hide");
+  projectsDivContent.classList.add("hide");
+  softSKillDivCont.classList.remove("hide");
+});
